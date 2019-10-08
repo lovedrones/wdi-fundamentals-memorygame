6cb0dc95895d19f9c0f30e4f6b14a3a5485c4310
Juan Lo <JuanTron@juans-mbp.attlocal.net>
2019-10-08 06:03:40 -0500

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,8 +1,21 @@
-console.log("Up and running!");
 
-var cardOne = "queen";
-var cardTwo = "king";
-var cardThree = "queen";
-var cardFour = "king";
 
-console.log("User flipped " + cardOne);
+const cards = ["queen", "queen", "king", "king"];
+const cardsInPlay = [""];
+var cardOne = cards[0]; 
+cardsInPlay.push('cardOne');
+console.log("User flipped Queen");
+
+var cardTwo = cards[3];
+cardsInPlay.push('cardTwo');
+console.log("User flipped king");
+
+if (/* cardsInPlay.length === 2; */){
+	else if(/* cardsInPlay[0] === cardsInPlay[3]*/)
+}
+alert("You found a match!");
+else if
+
+else if(/* cardsInPlay[0] === cardsInPlay[3]*/)
+}
+() alert("Sorry, try again.");
